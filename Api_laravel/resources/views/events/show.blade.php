df432dab086fcca1731b13f5847d2cdf18cb2d77
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2023-10-02 15:12:09 -0300

    Update show.blade.php

--- Api_laravel/resources/views/events/show.blade.php
+++ Api_laravel/resources/views/events/show.blade.php
@@ -9,6 +9,7 @@
             <div id="image-container" class="col-md-6">
                 <img  src="/img/events/{{$event->image}}" class="img-fluid" alt="{{$event->title}}">
             </div>
+            <!---->
             <div id="info-container" class="col-md-6">
                 <h1>{{$event->title}}</h1>
                 <p class="event-city"><ion-icon name="location-outline"></ion-icon>{{$event->city}}</p>
